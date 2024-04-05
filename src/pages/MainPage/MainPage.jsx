4f2a2d2a6ff1f62188e6ff436bbf5a1fd8609424
adam <adam.erben@seznam.cz>
2024-04-05 12:08:24 +0200

    radio

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -6,7 +6,13 @@ export default function MainPage() {
     <>
       <div id="container">
         <form>
-        <input id="input" type="text" placeholder="Gang Gang"/>
+        <input id="input" type="text" placeholder="Gang Gang"/><br />   
+
+        <br />
+        <input type="radio" name="position" id="teacher" value="Teacher" />
+        <label for="teacher">Teacher</label><br />
+        <input type="radio" name="position" id="student" value="Student" />
+        <label for="student">Student</label><br />
         <button id="btn">SEND</button>
         </form>
         
